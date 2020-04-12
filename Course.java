f78b01a4a906e16e3b808aee240facc46431b4bf
igorletnikov <letnikov46@gmail.com>
2020-04-12 15:49:37 +0200

    updates changed

---
+++ Course.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Course {
+    private String courseName;
+    private int noOfStudents;
+    private String teacher;
+    public static int instances = 0;
+
+    //Getters
+    public String getCourseName(){
+        return this.courseName;
+    }
+    public int getNoOfStudents(){
+        return this.noOfStudents;
+    }
+    public String getTeacher(){
+        return this.teacher;
+    }
+
+    //Setters
+    public void setCourseName(String courseName){
+        this.courseName = courseName;
+    }
+    public void setNoOfStudents(int noOfStudents){
+        this.noOfStudents = noOfStudents;
+    }
+    public void setTeacher(String teacher){
+        this.teacher = teacher;
+    }
+
+    public Course(){
+        instances++;
+        this.noOfStudents = 0;
+        this.courseName = "Not Set";
+        this.teacher = "Not Set";
+    }
+
+    public Course(int noOfStudents, String courseName, String teacher){
+        this.noOfStudents = noOfStudents;
+        this.courseName = courseName;
+        this.teacher = teacher;
+    }
+
+}
